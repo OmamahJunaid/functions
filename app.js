42db3e8c290db63f64522d1953da43a18fd9bf46
omamah junaid <junaidomamah@gmail.com>
2023-10-06 15:18:22 +0500

    add all files

---
+++ app.js
@@ -0,0 +1,88 @@
+// **********************Functions**********************************
+// // Q1
+// function todayDate(){
+//     var today = new Date()
+
+// document.write(today)
+// }
+// todayDate()
+// // Q2
+// function greet() {
+//   var firstname = prompt("Enter first name");
+//   var lastname = prompt("Enter last name");
+//   var fullname = firstname + lastname;
+//   document.write( "welcome "+fullname);
+// }
+// greet();
+// // Q3
+// function sum(numOne,numTwo){
+ 
+//     return numOne+numTwo
+// }
+// var numOne=+prompt("Enter a number")
+// var numTwo=+prompt("Enter a number")
+// var res = sum(numOne,numTwo)
+// console.log(res)
+// // Q4
+// function calculator(num1, opr, num2) {
+//   if (opr === "+") {
+//     return num1 + num2;
+//   } else if (opr === "-") {
+//     return num1 - num2;
+//   } else if (opr === "*") {
+//     return num1 * num2;
+//   } else if (opr === "/") {
+//     return num1 / num2;
+//   }
+// }
+
+// var num1 = prompt("Enter first value");
+// var opr = prompt("Enter operator value");
+// var num2 = prompt("Enter Second value");
+
+// var result = calculator(num1, opr, num2);
+
+// console.log(result);
+// // Q5
+// function sqr(num){
+//     return num*num
+// }
+// var num=+prompt("Enter a number")
+// var result = sqr(num)
+// console.log(result)
+
+// // Q6
+// function factorial(numf){
+//     for (i = 1; i <= numf; i++) {
+//        numf*= i;
+//     }
+// }
+// var numf=+prompt("Enter a number")
+//  factorial(numf)
+// console.log( factorial(numf))
+// // // Q7
+// function count(num1,num2){
+//     for(i=num1;i<=num2; i++){
+//         console.log(i)
+//     }
+// }
+// var num1 =+prompt("Enter starting number")
+// var num2 =+prompt("Enter ending number")
+// count(num1,num2)
+// // Q9
+// function area( width,height){
+//     return width * height
+// }
+// var width =+prompt("Enter width")
+// var height=+prompt("Enter height")
+// var res = area(width,height)
+// console.log(res)
+
+// // 10
+
+
+// 11
+
+// 12
+// 13
+// 14
